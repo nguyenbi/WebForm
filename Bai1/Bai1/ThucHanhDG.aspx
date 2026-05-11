c516f7b242be924bde492f1ca6255ac125607e64
Windows <Windows@WINDOWS-PC>
2026-05-11 20:51:43 +0700

    Add ThucHanh

---
+++ Bai1/Bai1/ThucHanhDG.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ThucHanhDG.aspx.cs" Inherits="Bai1.ThucHanhDG" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DataGrid ID="grNhanVien" runat="server" AutoGenerateColumns="false" OnItemCommand="grNhanVien_ItemCommand">
+                <Columns>
+                    <asp:TemplateColumn HeaderText="ID">
+                        <ItemTemplate>
+                             <%# DataBinder.Eval(Container.DataItem, "ID") %>
+                        </ItemTemplate>
+                    </asp:TemplateColumn>
+
+                    <asp:TemplateColumn HeaderText="Name">
+
+                        <ItemTemplate>
+                           <%# DataBinder.Eval(Container.DataItem, "Name") %>
+                        </ItemTemplate>
+
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtName" runat="server" Text='<%# DataBinder.Eval(Container.DataItem, "Name") %>'></asp:TextBox>
+                            <br /><asp:Label ID="lblErrorName" runat="server" ForeColor="Red"></asp:Label>
+                        </EditItemTemplate>
+
+                    </asp:TemplateColumn>
+
+                    <asp:TemplateColumn HeaderText="Dien Thoai">
+                        <ItemTemplate>
+                            <%# DataBinder.Eval(Container.DataItem,"Phone") %>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtPhone" runat="server" Text='<%# DataBinder.Eval(Container.DataItem,"Phone") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                    </asp:TemplateColumn>
+
+                    <asp:TemplateColumn HeaderText="Address">
+                        <ItemTemplate><%# DataBinder.Eval(Container.DataItem, "Address") %></ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtAddress" runat="server" Text='<%# DataBinder.Eval(Container.DataItem, "Address") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                    </asp:TemplateColumn>
+
+                    <asp:TemplateColumn HeaderText="Action">
+                        <ItemTemplate>
+                            <asp:LinkButton runat="server" CommandName="Edit" Text="Sua"></asp:LinkButton>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:LinkButton runat="server" CommandName="Update" Text="Luu"></asp:LinkButton>
+                            <asp:LinkButton runat="server" CommandName="Cancel" Text="hủy"></asp:LinkButton>
+                        </EditItemTemplate>
+                    </asp:TemplateColumn>
+
+                </Columns>
+            </asp:DataGrid>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Bai1/Bai1/ThucHanhDG.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bai1
+{
+    public partial class ThucHanhDG : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadDataGRNhanVien();
+            }
+        }
+
+        protected void LoadDataGRNhanVien()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Phone");
+            dt.Columns.Add("Address");
+
+            // Lay tu DB
+            // Select * from tblNhanVien
+            dt.Rows.Add("1", "Nguyen van A", "0546565", "123 Da Nang");
+            dt.Rows.Add("2", "Nguyen van B", "0546565", "123 Da Nang");
+            dt.Rows.Add("3", "Nguyen van C", "0546565", "123 SG");
+            dt.Rows.Add("4", "Nguyen van D", "0546565", "123 Da Nang");
+            dt.Rows.Add("5", "Nguyen van E", "0546565", "123 Can tho");
+
+            grNhanVien.DataSource = dt;
+            grNhanVien.DataBind();
+        }
+
+        protected void grNhanVien_ItemCommand(object source, DataGridCommandEventArgs e)
+        {
+            if (e.CommandName == "Edit")
+            {
+                grNhanVien.EditItemIndex = e.Item.ItemIndex;
+                LoadDataGRNhanVien();
+            }
+            if (e.CommandName == "Update")
+            {
+                TextBox txtName = (TextBox)e.Item.FindControl("txtName");
+                string name = txtName.Text;
+                // 
+                Label lblErrorName = (Label)e.Item.FindControl("lblErrorName");
+                if(name.Length > 20)
+                {
+                  lblErrorName.Text = "Ten khong duoc vuot qua 20";
+                }
+
+
+                TextBox txtPhone = (TextBox)e.Item.FindControl("txtPhone");
+                string phone = txtPhone.Text;
+                /// Viet ham validate phone
+
+                TextBox txtAdd = (TextBox)e.Item.FindControl("txtAddress");
+                string diaChi = txtAdd.Text;
+
+                // Luu DB
+                // Insert DB
+                ///
+
+                grNhanVien.EditItemIndex = -1;
+                LoadDataGRNhanVien();
+            }
+
+            if(e.CommandName =="Cancel")
+            {
+                grNhanVien.EditItemIndex = -1;
+                // Load lai DB
+                LoadDataGRNhanVien();
+            }
+
+            switch (e.CommandName)
+            {
+                case "Edit":
+                    {
+                        grNhanVien.EditItemIndex = e.Item.ItemIndex;
+                        LoadDataGRNhanVien();
+                        break;
+                    }
+                case "Update":
+                    {
+
+                        break;
+                    }
+                case "Cancel":
+                    {
+
+                        break;
+                    }
+            }
+        }
+    }
+}
